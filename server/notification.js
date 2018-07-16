fcfafd1726d30761e96409146431cb98af7cfac9
Ahmad Ataya <ahmad.3taya@gmail.com>
2018-07-16 22:52:32 +0300

    edit server ip

--- server/notification.js
+++ server/notification.js
@@ -20,11 +20,26 @@ module.exports.addNewVolume = function (VolumesModel, volume) {
 	})
 	VolumesModel.getDataSource().connector.collection('user').aggregate([
 		{ $match: {postCategoriesIds : {$in : allCategories}} },
-		{ $project: { postCategoriesIds: 1, commonToBoth: { $setIntersection: [ "$postCategoriesIds", allCategories ] }, _id: 0 } }
+		{ $project: { postCategoriesIds: 1, commonToBoth: { $setIntersection: [ "$postCategoriesIds", allCategories ] }} }
                      
      ],function(err,users){
-		console.log(err,users);
+     	if(err)  // TODO Debug
+     		return console.log(err);
+     	_.each(users,function(user){
+     		console.log(user)
+     		_sendNotification(user._id)
+     	});
      })
+}
+
+
+var sendNotificationToMultiUsers = function(usersIds,message){
+	_.each(usersIds,(userId)=>{_sendNotification(userId,message)});
+}
+
+var _sendNotification = function(userId,message){
+	console.log(userId,message);
+	// myClient.
 }
 
 // module.exports = myClient;
