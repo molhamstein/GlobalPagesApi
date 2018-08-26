dc1da3b512194094f1691ff3e1e17c6e83f537db
Ahmad Ataya <ahmad.3taya@gmail.com>
2018-08-26 10:51:36 +0300

    T

--- server/boot/GenerateData.js
+++ server/boot/GenerateData.js
@@ -2,8 +2,9 @@
 
 module.exports = function GenerateData(server) {
 // users 
-
-/*
+/******************************************************************************
+// return;
+******************************************************************************/
 	var users = [
 		{
 		  "username": "ahmad ataya",
@@ -75,131 +76,164 @@ createData('user',users,function(err,users){
 			if(err)
 				return console.log(err);
 
-			var posts = [
-				{
-				  "title": "mercedes E350",
-				  "description": "نضيفة خالية برا جوا زنار نضافة",
-				  "status": "activated",
-				  "image": "m1.jpg",
-				  "viewsCount": 3,
-				  "ownerId": users[0].id.toString(),
-				  "categoryId": categories[0].id.toString(),
-				  "subCategoryId": childCategories[1].id.toString()
-				},
-				{
-				  "title": "mercedes E650",
-				  "description": "bla bla bla bla",
-				  "status": "activated",
-				  "image": "m1.jpg",
-				  "viewsCount": 6,
-				  "ownerId": users[0].id.toString(),
-				  "categoryId": categories[0].id.toString(),
-				  "subCategoryId": childCategories[1].id.toString()
-				},
-				{
-				  "title": "BMW x5",
-				  "description": "bla bla bla bla",
-				  "status": "activated",
-				  "image": "m1.jpg",
-				  "viewsCount": 12,
-				  "ownerId": users[0].id.toString(),
-				  "categoryId": categories[0].id.toString(),
-				  "subCategoryId": childCategories[0].id.toString()
-				},
-				{
-				  "title": "ipone X",
-				  "description": "bla bla bla bla",
-				  "status": "activated",
-				  "image": "m1.jpg",
-				  "viewsCount": 2,
-				  "ownerId": users[0].id.toString(),
-				  "categoryId": categories[1].id.toString(),
-				  "subCategoryId": childCategories[2].id.toString()
-				},
-				{
-				  "title": "hwawei mate 10 lite",
-				  "description": "bla bla bla bla",
-				  "status": "activated",
-				  "image": "m1.jpg",
-				  "viewsCount": 0,
-				  "ownerId": users[1].id.toString(),
-				  "categoryId": categories[1].id.toString()
-				}
-			]
-
-			createData('posts',posts,function(err,POSTS){
-				if(err)
-					return console.log(err);
-
-				var volumes = [
-					{
-					  "titleEn": "Title Volume",
-					  "titleAr": "المجلة الأسبوعية",
-					  "status": "activated",
-					  "postsIds": [
-					    POSTS[0].id.toString(),
-					    POSTS[1].id.toString(),
-					    POSTS[2].id.toString(),
-					    POSTS[3].id.toString(),
-					    POSTS[4].id.toString()
-					  ]
-					}
-				];
-				createData('volumes',volumes,function(err,VOLUMES){
+				createData('cities',cities,function(err,cities){
 					if(err)
 						return console.log(err);
-
-					var businessCategories = [
-						{
-						  "titleEn": "Restaurants",
-						  "titleAr": "bla bla"
-						},
-						{
-						  "titleEn": "Medicine",
-						  "titleAr": "bla bla"
-						}	
-					]
-					createData('businessCategories',businessCategories,function(err,businessCategories){
+					createData('locations',locations,function(err,locations){
 						if(err)
 							return console.log(err);
-						var businesChildCategories = [
+
+						var posts = [
 							{
-							  "titleEn": "Eyes",
-							  "titleAr": "bla bla",
-							  "parentCategoryId" : businessCategories[1].id.toString()
+							  "title": "mercedes E350",
+							  "description": "نضيفة خالية برا جوا زنار نضافة",
+							  "status": "activated",
+							  "viewsCount": 3,
+							  "ownerId": users[0].id.toString(),
+							  "categoryId": categories[0].id.toString(),
+							  "subCategoryId": childCategories[1].id.toString(),
+							  "cityId" : cities[0].id.toString(),
+							  "locationId" : locations[0].id.toString(),
+							  "media": [
+									{
+										"url": "104.217.253.15:3000/images/testImage1.jpg",
+										"type": "image",
+										"id": "1"
+									},
+									{
+										"url": "104.217.253.15:3000/images/testImage2.jpg",
+										"type": "image",
+										"id": "2"
+									}
+								]
 							},
 							{
-							  "titleEn": "Plastic surgery",
-							  "titleAr": "bla bla",
-							  "parentCategoryId" : businessCategories[1].id.toString()
+							  "title": "mercedes E650",
+							  "description": "bla bla bla bla",
+							  "status": "activated",
+							  "image": "m1.jpg",
+							  "viewsCount": 6,
+							  "ownerId": users[0].id.toString(),
+							  "categoryId": categories[0].id.toString(),
+							  "subCategoryId": childCategories[1].id.toString(),
+							  "cityId" : cities[0].id.toString(),
+							  "locationId" : locations[1].id.toString(),
+							  "media": [
+									{
+										"url": "104.217.253.15:3000/images/testImage1.jpg",
+										"type": "image",
+										"id": "1"
+									}
+								]
 							},
 							{
-							  "titleEn": "caffe",
-							  "titleAr": "bla bla",
-							  "parentCategoryId" : businessCategories[0].id.toString()
+							  "title": "BMW x5",
+							  "description": "bla bla bla bla",
+							  "status": "activated",
+							  "image": "m1.jpg",
+							  "viewsCount": 12,
+							  "ownerId": users[0].id.toString(),
+							  "categoryId": categories[0].id.toString(),
+							  "subCategoryId": childCategories[0].id.toString(),
+							  "cityId" : cities[1].id.toString()
 							},
 							{
-							  "titleEn": "dinner",
-							  "titleAr": "bla bla",
-							  "parentCategoryId" : businessCategories[0].id.toString()
+							  "title": "ipone X",
+							  "description": "bla bla bla bla",
+							  "status": "activated",
+							  "image": "m1.jpg",
+							  "viewsCount": 2,
+							  "ownerId": users[0].id.toString(),
+							  "categoryId": categories[1].id.toString(),
+							  "subCategoryId": childCategories[2].id.toString(),
+							  "cityId" : cities[0].id.toString(),
+							  "locationId" : locations[2].id.toString(),
+							  "media": [
+									{
+										"url": "104.217.253.15:3000/images/testImage1.jpg",
+										"type": "image",
+										"id": "1"
+									}
+								]
 							},
 							{
-							  "titleEn": "fast food",
-							  "titleAr": "bla bla",
-							  "parentCategoryId" : businessCategories[0].id.toString()
+							  "title": "hwawei mate 10 lite",
+							  "description": "bla bla bla bla",
+							  "status": "activated",
+							  "image": "m1.jpg",
+							  "viewsCount": 0,
+							  "ownerId": users[1].id.toString(),
+							  "categoryId": categories[1].id.toString(),
+							  "cityId" : cities[2].id.toString()
 							}
-						];
-						
-						createData('businessCategories',businesChildCategories,function(err,businesChildCategories){
+						]
+
+						createData('posts',posts,function(err,POSTS){
 							if(err)
 								return console.log(err);
 
-							createData('cities',cities,function(err,cities){
+							var volumes = [
+								{
+								  "titleEn": "Title Volume",
+								  "titleAr": "المجلة الأسبوعية",
+								  "status": "activated",
+								  "postsIds": [
+								    POSTS[0].id.toString(),
+								    POSTS[1].id.toString(),
+								    POSTS[2].id.toString(),
+								    POSTS[3].id.toString(),
+								    POSTS[4].id.toString()
+								  ]
+								}
+							];
+							createData('volumes',volumes,function(err,VOLUMES){
 								if(err)
 									return console.log(err);
-								createData('locations',locations,function(err,locations){
+
+								var businessCategories = [
+									{
+									  "titleEn": "Restaurants",
+									  "titleAr": "bla bla"
+									},
+									{
+									  "titleEn": "Medicine",
+									  "titleAr": "bla bla"
+									}	
+								]
+								createData('businessCategories',businessCategories,function(err,businessCategories){
 									if(err)
 										return console.log(err);
+									var businesChildCategories = [
+										{
+										  "titleEn": "Eyes",
+										  "titleAr": "bla bla",
+										  "parentCategoryId" : businessCategories[1].id.toString()
+										},
+										{
+										  "titleEn": "Plastic surgery",
+										  "titleAr": "bla bla",
+										  "parentCategoryId" : businessCategories[1].id.toString()
+										},
+										{
+										  "titleEn": "caffe",
+										  "titleAr": "bla bla",
+										  "parentCategoryId" : businessCategories[0].id.toString()
+										},
+										{
+										  "titleEn": "dinner",
+										  "titleAr": "bla bla",
+										  "parentCategoryId" : businessCategories[0].id.toString()
+										},
+										{
+										  "titleEn": "fast food",
+										  "titleAr": "bla bla",
+										  "parentCategoryId" : businessCategories[0].id.toString()
+										}
+									];
+									
+									createData('businessCategories',businesChildCategories,function(err,businesChildCategories){
+										if(err)
+											return console.log(err);
 
 									var business = [
 										{
@@ -211,7 +245,7 @@ createData('user',users,function(err,users){
 											"description": "bla bla bla bla bla bla bla bla bla ",
 											"locationPoint" : {
 												"lat": 12,
-												"lng": 255
+												"lng": 12
 											},
 											"openingDays": [
 											1,2,5
@@ -279,7 +313,6 @@ var products = [
 
 ];
 
-*/
 
 
 	
