4c9b04ab1624a00519f390e42b3dd075db0b0e06
ahmad-ataya <ahmad.3taya@gmail.com>
2018-10-13 13:42:11 +0300

    T'

---
+++ server/boot/addDefaultData.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = function addDefaultData(server) {
+	addDefaultBusinessCategories();
+
+
+	function addDefaultBusinessCategories(){
+		server.models.businessCategories.findOrCreate({where : {code : 'medicine'}},{
+			code : "medicine", 
+			titleAr : "طب",
+			titleEn : "medicine",
+		},function(err,medicine){
+			if(err)
+				return console.log(err);
+			server.models.businessCategories.findOrCreate({where : {code : 'pharmacies'}},{
+				code : "pharmacies", 
+				titleAr : "صيدليات",
+				titleEn : "pharmacies",
+				parentCategoryId : medicine.id
+			},function(err,pharmacies){
+				if(err)
+					return console.log(err);
+			});
+		});
+	}
+};
