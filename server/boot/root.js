1da94ac74f9b7fce4e4a9c36353547b75e213b82
ahmad-ataya <ahmad.3taya@gmail.com>
2019-03-14 17:31:28 +0200

    T

--- server/boot/root.js
+++ server/boot/root.js
@@ -2,6 +2,14 @@
 
 module.exports = function(server) {
   // Install a `/` route that returns server status
+
+   global.ERROR = function(statusCode,message,code){
+  	var err = new Error(message);
+  	err.statusCode = statusCode;
+  	err.code = code || (message.replace(/ /g, '_').toUpperCase());
+  	return err;
+  }
+  
   var router = server.loopback.Router();
   router.get('/', server.loopback.status());
   server.use(router);
