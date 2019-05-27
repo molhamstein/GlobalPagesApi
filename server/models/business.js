fc1fc56763409f9961857e58c12ca7a8668a69e9
Rami Al-Zebak <17zrami@gmail.com>
2019-05-27 16:44:18 +0300

    safe delete

--- server/models/business.js
+++ server/models/business.js
@@ -1,145 +1,176 @@
 'use strict';
 var _ = require('lodash');
-module.exports = function(Business) {
+module.exports = function (Business) {
 	// validation
-	Business.validatesInclusionOf('status', {in: ['pending', 'activated','deactivated']});
+	Business.validatesInclusionOf('status', { in: ['pending', 'activated', 'deactivated'] });
 	Business.validatesPresenceOf('categoryId');
 
 	// add ownerId to Business
-	Business.beforeRemote('create', function( ctx, modelInstance, next) {
-		if(!ctx.req.body.ownerId)
+	Business.beforeRemote('create', function (ctx, modelInstance, next) {
+		if (!ctx.req.body.ownerId)
 			ctx.req.body.ownerId = ctx.args.options.accessToken.userId;
-		var name = ctx.req.body.nameEn.trim().replace(/ * /g,'.');
-		
+		var name = ctx.req.body.nameEn.trim().replace(/ * /g, '.');
+
 		// make unique name
-		Business.count({nameUnique : {like : RegExp('^' + name + '[0-9]*')}},function(err,count){
-			if(err)
+		Business.count({ nameUnique: { like: RegExp('^' + name + '[0-9]*') } }, function (err, count) {
+			if (err)
 				return next(err);
-			
+
 			ctx.req.body.nameUnique = name;
-			if(count != 0)
+			if (count != 0)
 				ctx.req.body.nameUnique = name + (count.toString())
 
 			var tempId = 1;
-			_.each(ctx.req.body.covers,(m)=>{m.id= tempId++});
+			_.each(ctx.req.body.covers, (m) => { m.id = tempId++ });
 			var tempId = 1;
-			_.each(ctx.req.body.products,(m)=>{m.id= tempId++});
+			_.each(ctx.req.body.products, (m) => { m.id = tempId++ });
 
-	    	next();
+			next();
 		});
 	});
 
-	Business.beforeRemote('replaceById', function( ctx, modelInstance, next) {
-		if(!ctx.req.body.ownerId)
+	Business.beforeRemote('replaceById', function (ctx, modelInstance, next) {
+		if (!ctx.req.body.ownerId)
 			ctx.req.body.ownerId = ctx.args.options.accessToken.userId
 		var tempId = 1;
-		_.each(ctx.req.body.covers,(m)=>{m.id= tempId++});
+		_.each(ctx.req.body.covers, (m) => { m.id = tempId++ });
 		var tempId = 1;
-		_.each(ctx.req.body.products,(m)=>{m.id= tempId++});
-	    next();
+		_.each(ctx.req.body.products, (m) => { m.id = tempId++ });
+		next();
 	});
 
 	// agree or reject Business
-	Business.changeStatus = function(businessId,status,cb){
-		Business.findById(businessId.toString(),{}, function(err, business) {
-			if(err) 
+	Business.changeStatus = function (businessId, status, cb) {
+		Business.findById(businessId.toString(), {}, function (err, business) {
+			if (err)
 				return cb(err);
-			if(!business){
+			if (!business) {
 				err = new Error('usiness not found');
-		        err.statusCode = 404;
-		        err.code = 'BUSINESS_NOT_FOUND';
-		        return cb(err);
+				err.statusCode = 404;
+				err.code = 'BUSINESS_NOT_FOUND';
+				return cb(err);
 			}
-			business.status = (status)?'activated':'deactivated';
-			business.save((err)=>{
-				if(err)
+			business.status = (status) ? 'activated' : 'deactivated';
+			business.save((err) => {
+				if (err)
 					return cb(err)
 				return cb(null, business.status);
 			})
 		});
 	}
 	Business.remoteMethod('changeStatus', {
-    	description: 'agree or Reject business from admin',
+		description: 'agree or Reject business from admin',
 		accepts: [
-			{arg: 'businessId', type: 'string',  required:true},
-			{arg: 'agree', type: 'boolean', required: true, http: {source: 'body'}},
+			{ arg: 'businessId', type: 'string', required: true },
+			{ arg: 'agree', type: 'boolean', required: true, http: { source: 'body' } },
 		],
-		returns: {arg: 'message', type: 'string'},
-		http: {verb: 'post',path: '/:businessId/changeStatus'},
-    });
+		returns: { arg: 'message', type: 'string' },
+		http: { verb: 'post', path: '/:businessId/changeStatus' },
+	});
 
 	// to get busniess By nameUnique
-	Business.afterRemote('findById', function(ctx,result, next) {
-		if(result)
-			return next();
 
-		Business.findOne({where: {nameUnique : ctx.args.id}},function(err,busniess){
-			if(err  || !busniess)
-				return next(err);
-	  		return ctx.res.json(busniess)
-		})
+
+	Business.afterRemote('findById', async function (ctx) {
+
+
+		if (!ctx.result) {
+			ctx.result = await Business.findOne({ where: { nameUnique: ctx.args.id } });
+		}
+
+	});
+
+	// execlude deleted entities 
+	Business.observe('access', async function (ctx) {
+
+		ctx.query = ctx.query || {};
+		ctx.query.where = ctx.query.where || {};
+		if (!ctx.query.where.deleted) {
+			ctx.query.where.deleted = false;
+		}
+	});
+
+
+	Business.once("attached", function () {
+
+
+		Business.deleteById = async function (id, auth, cb) {
+
+
+			let business = await Business.findById(id);
+			if (!business)
+				throw { message: "Resource not found", code: 404 };
+
+
+			business.updateAttribute("deleted", true);
+
+
+			return "deleted";
+		}
+
+
 	});
 
 
 
 
-	Business.searchByLocation = function(lat,lng,keyword,catId,subCatId,codeCat,codeSubCat,openingDay,limit,res,cb){
+	Business.searchByLocation = function (lat, lng, keyword, catId, subCatId, codeCat, codeSubCat, openingDay, limit, res, cb) {
 		var where = {
 			// status : 'activated'
 		};
 
-		if(lat) 
-			_.set(where,'locationPoint.near.lat',lat);
-		if(lng) 
-			_.set(where,'locationPoint.near.lng',lng);
-		if(catId) where.categoryId = catId;
-		if(subCatId) where.subCategoryId = subCatId;
-		if(keyword) where.or = [
-			{nameEn :{like : RegExp(keyword)}},
-			{nameAr :{like : RegExp(keyword)}},
-			{nameUnique :{like : RegExp(keyword)}},
-			{description :{like : RegExp(keyword)}}
+		if (lat)
+			_.set(where, 'locationPoint.near.lat', lat);
+		if (lng)
+			_.set(where, 'locationPoint.near.lng', lng);
+		if (catId) where.categoryId = catId;
+		if (subCatId) where.subCategoryId = subCatId;
+		if (keyword) where.or = [
+			{ nameEn: { like: RegExp(keyword) } },
+			{ nameAr: { like: RegExp(keyword) } },
+			{ nameUnique: { like: RegExp(keyword) } },
+			{ description: { like: RegExp(keyword) } }
 		]
-		if(openingDay){
+		if (openingDay) {
 			where.openingDaysEnabled = true;
 			where.openingDays = openingDay;
 		}
 
-		if((lat && !lng) || (lng && !lat)){
+		if ((lat && !lng) || (lng && !lat)) {
 			var err = new Error('lat and lng both required');
-	        err.statusCode = 400;
-	        err.code = 'LATLNGREQUIRED';
-	        return cb(err);
+			err.statusCode = 400;
+			err.code = 'LATLNGREQUIRED';
+			return cb(err);
 		}
-			
-		if(lat && lat > 90){
+
+		if (lat && lat > 90) {
 			var err = new Error('lat must be <= 90');
-	        err.statusCode = 400;
-	        err.code = 'LAT>90';
-	        return cb(err);
+			err.statusCode = 400;
+			err.code = 'LAT>90';
+			return cb(err);
 		}
-		if(lng && lng > 180){
+		if (lng && lng > 180) {
 			var err = new Error('lng must be <= 180');
-	        err.statusCode = 400;
-	        err.code = 'LNG>180';
-	        return cb(err);
+			err.statusCode = 400;
+			err.code = 'LNG>180';
+			return cb(err);
 		}
 
-		getCategorybyCode(codeCat,(err,category)=>{
-			if(err)
+		getCategorybyCode(codeCat, (err, category) => {
+			if (err)
 				return cb(err);
-			if(category) where.categoryId = category.id;
+			if (category) where.categoryId = category.id;
 
-			getCategorybyCode(codeSubCat,(err,subCategory)=>{
-				if(err)
+			getCategorybyCode(codeSubCat, (err, subCategory) => {
+				if (err)
 					return cb(err);
-				if(subCategory) where.subCategoryId = subCategory.id;
-				
-				var query = {where : where};
+				if (subCategory) where.subCategoryId = subCategory.id;
+
+				var query = { where: where };
 				console.log(where);
-				if(limit) query.limit = limit;
-				Business.find(query,function(err,business){
-					if(err) 
+				if (limit) query.limit = limit;
+				Business.find(query, function (err, business) {
+					if (err)
 						return cb(err);
 					return res.status(200).json(business)
 				});
@@ -147,26 +178,26 @@ module.exports = function(Business) {
 		});
 	}
 	Business.remoteMethod('searchByLocation', {
-    	// description: '',
+		// description: '',
 		accepts: [
-			{arg: 'lat', type: 'number', http: {source: 'query'}},
-			{arg: 'lng', type: 'number', http: {source: 'query'}},
-			{arg: 'keyword', type: 'string', http: {source: 'query'}},
-			{arg: 'catId', type: 'string', http: {source: 'query'}},
-			{arg: 'subCatId', type: 'string', http: {source: 'query'}},
-			{arg: 'codeCat', type: 'string', http: {source: 'query'}},
-			{arg: 'codeSubCat', type: 'string', http: {source: 'query'}},
-			{arg: 'openingDay', type: 'number', http: {source: 'query'}},
-			{arg: 'limit', type: 'number', http: {source: 'query'}},
-			{arg: 'res', type: 'object', http:{source:'res'}},
+			{ arg: 'lat', type: 'number', http: { source: 'query' } },
+			{ arg: 'lng', type: 'number', http: { source: 'query' } },
+			{ arg: 'keyword', type: 'string', http: { source: 'query' } },
+			{ arg: 'catId', type: 'string', http: { source: 'query' } },
+			{ arg: 'subCatId', type: 'string', http: { source: 'query' } },
+			{ arg: 'codeCat', type: 'string', http: { source: 'query' } },
+			{ arg: 'codeSubCat', type: 'string', http: { source: 'query' } },
+			{ arg: 'openingDay', type: 'number', http: { source: 'query' } },
+			{ arg: 'limit', type: 'number', http: { source: 'query' } },
+			{ arg: 'res', type: 'object', http: { source: 'res' } },
 		],
-		http: {verb: 'get',path: '/searchByLocation'},
-    });
+		http: { verb: 'get', path: '/searchByLocation' },
+	});
 
 
-    var getCategorybyCode = function(code,cb){
-    	if(!code || code == 'default')
-    		return cb(null,null);
-    	return Business.app.models.businessCategories.findOne({where : {code : code}},cb);
-    }
+	var getCategorybyCode = function (code, cb) {
+		if (!code || code == 'default')
+			return cb(null, null);
+		return Business.app.models.businessCategories.findOne({ where: { code: code } }, cb);
+	}
 };
--- server/models/cities.js
+++ server/models/cities.js
@@ -1,18 +1,72 @@
 'use strict';
 var _ = require('lodash');
-module.exports = function(Cities) {
-	Cities.afterRemote('create', function( ctx, result, next) {
+module.exports = function (Cities) {
+	Cities.afterRemote('create', function (ctx, result, next) {
 		var locations = ctx.req.body.locations || []
-		_.each(locations,(loc) =>{
+		_.each(locations, (loc) => {
 			loc.cityId = result.id
 		});
 
 
-		Cities.app.models.locations.create(locations,function(err,locations){
-			if(err) 
+		Cities.app.models.locations.create(locations, function (err, locations) {
+			if (err)
 				return next(err);
 			// result.locations = locations;
 			return next();
 		});
 	});
+
+
+
+	Cities.once("attached", function () {
+
+
+		Cities.deleteById = async function (id, auth, cb) {
+
+
+			let city = await Cities.findById(id);
+			if (!city)
+				throw { message: "Resource not found", code: 404 };
+
+
+			// delete posts
+			for (let post of await city.posts.find()) {
+				post.updateAttribute("deleted", true);
+			}
+
+			// delete businesses
+			for (let business of await city.businesses.find()) {
+				business.updateAttribute("deleted", true);
+			}
+
+
+			//	delete locations
+			for (let location of await city.locations.find()) {
+				location.updateAttribute("deleted", true);
+			}
+
+			//delete self
+			category.updateAttribute("deleted", true);
+
+			return "deleted";
+		}
+
+
+	});
+
+
+
+	
+	
+	// execlude deleted entities 
+	Cities.observe('access', async function (ctx) {
+
+		ctx.query = ctx.query || {};
+		ctx.query.where = ctx.query.where || {};
+		if (!ctx.query.where.deleted) {
+			ctx.query.where.deleted = false;
+		}
+	});
+
+
 };
