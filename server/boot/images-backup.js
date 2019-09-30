b1c9a9884333e18e084a06edfe3da66d8e648e57
Rami Al-Zebak <17zrami@gmail.com>
2019-09-30 18:45:35 +0300

    check if backup file exists

--- server/boot/images-backup.js
+++ server/boot/images-backup.js
@@ -7,27 +7,46 @@ module.exports = async function (app) {
     let exec = require('child_process').exec;
     let CronJob = require('cron').CronJob;
     let tar = require('tar');
-    let path = require("path");
+    let Path = require("path");
+    let config = {
 
-    let options = {
-
-        sourcePath: "files/",
+        sources: ["files/images", "files/thumb", "files/videos", "files/videos_thumb"],
         backupPath: "../backup/images-backup/"
 
     };
 
+    function getFiles(source) {
+        return fs.readdirSync(source).map(f => Path.join(source, f));
+    }
+
     async function imagesBackup() {
 
-        console.log("media backup");
+        console.log("images backup on progress .. ");
 
-        await tar.update({ file: path.join(options.backupPath ,  "images-backup.tar.gz") },  [options.sourcePath]);
-        
+        let files = config.sources.map(e => getFiles(e));
+        files = _.flatten(files);
+        let exists = [];
+        let tarFile = Path.join(config.backupPath, "images-backup.tar.gz");
+
+        if (fs.existsSync(tarFile)) {
+
+            tar.list({
+                sync: true,
+                file: Path.join(config.backupPath, "images-backup.tar.gz"),
+                onentry: entry => { exists.push(entry.path); }
+            });
+        }
+
+        files = files.filter(e => !exists.find(ex => ex == e));
+        if (files.length)
+            await tar.replace({ file: tarFile }, files);
+
         console.log("Done images backup");
 
 
     }
 
-    // every midnight 
+    // every midnight   
     let job = new CronJob('0 0 0 * * *', function () {
         imagesBackup();
     }, null, true);
