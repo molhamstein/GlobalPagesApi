d85a2ceedb8f5b99414f69a4a4d7db810e716329
anas-1995 <anasalazmeh.95@gmail.com>
2019-11-12 16:22:27 +0200

    solve notification

--- server/notification.js
+++ server/notification.js
@@ -102,7 +102,7 @@ module.exports.addNewVolume = async function (VolumesModel, volume) {
         });
       });
       _addAppNotificationToMultiUsers(appNotifications);
-      //  _sendNotificationToMultiTokens(fcmTokens,message,title,{volumeId : volume.id.toString()});
+       _sendNotificationToMultiTokens(fcmTokens,message,title,{volumeId : volume.id.toString()});
     });
   });
 
