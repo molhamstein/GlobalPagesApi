b9e600eded0154c86f7a932968d27a7cc478d407
anas-1995 <anasalazmeh.95@gmail.com>
2020-03-02 16:17:50 +0200

    add soft delete to product

--- server/models/products.js
+++ server/models/products.js
@@ -1,5 +1,37 @@
 'use strict';
 
-module.exports = function(Products) {
+module.exports = function (Products) {
+
+    Products.once("attached", function () {
+
+
+        Products.deleteById = async function (id, auth, cb) {
+
+
+            let product = await Products.findById(id);
+            if (!product)
+                throw {
+                    message: "Resource not found",
+                    code: 404
+                };
+
+
+            product.updateAttribute("deleted", true);
+
+
+            return "deleted";
+        }
+
+
+    });
+
+    Products.observe('access', async function (ctx) {
+
+        ctx.query = ctx.query || {};
+        ctx.query.where = ctx.query.where || {};
+        if (!ctx.query.where.deleted) {
+            ctx.query.where.deleted = false;
+        }
+    });
 
 };
