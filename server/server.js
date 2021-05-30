927c7af170eb04a0024140820087912eb42760f7
anas-1995 <anasalazmeh.95@gmail.com>
2021-05-30 11:35:23 +0300

    solve aggregate

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@
 
 var loopback = require('loopback');
 var boot = require('loopback-boot');
-// var loopbackSSL = require('loopback-ssl');
+var loopbackSSL = require('loopback-ssl');
 
 global.app = module.exports = loopback();
 const service = require('./boot/service.js');
